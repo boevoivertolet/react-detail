1b76e24a4a1a673f60f34cd6671b6afac4df9d86
boevoivertolet <bjha78rus@yandex.ru>
2022-05-02 15:26:51 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -48,4 +48,3 @@ const PageTitle = (props: PageTitlePropsType) => {
 
 
 export default App;
-
--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -1,81 +1,81 @@
 import React, {useState} from 'react';
 
 type RatingPropsType = {
-  //  value: 0 | 1 | 2 | 3 | 4 | 5
+    //  value: 0 | 1 | 2 | 3 | 4 | 5
 }
 
 export function UncontrolledRating(props: RatingPropsType) {
     console.log('UncontrolledRating rendering')
-let [value, setValue] = useState(0)
+    let [value, setValue] = useState(0)
 
 
-/*    if (props.value === 1) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-            </div>
-        )
+    /*    if (props.value === 1) {
+            return (
+                <div>
+                    <Star selected={true}/>
+                    <Star selected={false}/>
+                    <Star selected={false}/>
+                    <Star selected={false}/>
+                    <Star selected={false}/>
+                </div>
+            )
 
-    }
-    if (props.value === 2) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-            </div>
-        )
+        }
+        if (props.value === 2) {
+            return (
+                <div>
+                    <Star selected={true}/>
+                    <Star selected={true}/>
+                    <Star selected={false}/>
+                    <Star selected={false}/>
+                    <Star selected={false}/>
+                </div>
+            )
 
-    }
-    if (props.value === 3) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={false}/>
-                <Star selected={false}/>
-            </div>
-        )
+        }
+        if (props.value === 3) {
+            return (
+                <div>
+                    <Star selected={true}/>
+                    <Star selected={true}/>
+                    <Star selected={true}/>
+                    <Star selected={false}/>
+                    <Star selected={false}/>
+                </div>
+            )
 
-    }
-    if (props.value === 4) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={false}/>
-            </div>
-        )
+        }
+        if (props.value === 4) {
+            return (
+                <div>
+                    <Star selected={true}/>
+                    <Star selected={true}/>
+                    <Star selected={true}/>
+                    <Star selected={true}/>
+                    <Star selected={false}/>
+                </div>
+            )
 
-    }
-    if (props.value === 5) {
-        return (
-            <div>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-                <Star selected={true}/>
-            </div>
-        )
+        }
+        if (props.value === 5) {
+            return (
+                <div>
+                    <Star selected={true}/>
+                    <Star selected={true}/>
+                    <Star selected={true}/>
+                    <Star selected={true}/>
+                    <Star selected={true}/>
+                </div>
+            )
 
-    }*/
+        }*/
     return (
         <div>
-            <Star selected={value > 0}/><button onClick={()=> {setValue( 1)}}>1</button>
-            <Star selected={value > 1}/><button onClick={()=> {setValue( 2)}}>2</button>
-            <Star selected={value > 2}/><button onClick={()=> {setValue( 3)}}>3</button>
-            <Star selected={value > 3}/><button onClick={()=> {setValue( 4)}}>4</button>
-            <Star selected={value > 4}/><button onClick={()=> {setValue( 5)}}>5</button>
+            <Star selected={value > 0} setValue ={setValue}/>
+            <Star selected={value > 1} setValue ={setValue}/>
+            <Star selected={value > 2} setValue ={setValue}/>
+            <Star selected={value > 3} setValue ={setValue}/>
+            <Star selected={value > 4} setValue ={setValue}/>
 
         </div>
     )
@@ -84,14 +84,11 @@ let [value, setValue] = useState(0)
 
 type StarPropsType = {
     selected: boolean
+    setValue: (value :0 | 1 | 2 | 3 | 4 | 5) => void
 }
 
 function Star(props: StarPropsType) {
     console.log('Star rendering')
-    if (props.selected) {
-        return <span><b>star</b></span>
-    } else {
-        return <span>star</span>
-    }
+    return <span onClick={()=> {props.setValue(5)}}>{props.selected ? <b>star</b> : 'star'}</span>
 
 }
