6b8838847f64ab3b1f2b239a1880dc62e3c776f4
boevoivertolet <bjha78rus@yandex.ru>
2023-01-04 16:19:14 +0300

    useEffect(() => {         console.log('useEffect everytime render')         document.title = counter.toString()     })
        useEffect(() => {
            console.log('useEffect only first render(componentDidMount)')
            document.title = counter.toString()
        }, [])
    
        useEffect(() => {
            console.log('useEffect first render and every counter changed')
            document.title = counter.toString()
        }, [counter])

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -2,19 +2,36 @@ import React, {useEffect, useState} from 'react';
 
 export default {
 
-    title: 'UseEffect demo'
+    title: 'useEffect demo'
 }
 
 
 export const SimpleExample = () => {
     console.log('SimpleExample')
+    const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
 
     useEffect(() => {
-        console.log('useEffect')
+        console.log('useEffect everytime render')
+        document.title = counter.toString()
     })
+
+
+    useEffect(() => {
+        console.log('useEffect only first render(componentDidMount)')
+        document.title = counter.toString()
+    }, [])
+
+
+    useEffect(() => {
+        console.log('useEffect first render and every counter changed')
+        document.title = counter.toString()
+    }, [counter])
+
+
     return <>
-        Hello,{counter}
+        counter:{counter} fake:{fake}
+        <button onClick={() => setFake(fake + 1)}>+</button>
         <button onClick={() => setCounter(counter + 1)}>+</button>
     </>
 }
