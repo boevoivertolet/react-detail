6bcfe290e29e981ac49986a04e993520b56f0dcc
boevoivertolet <bjha78rus@yandex.ru>
2023-01-04 15:39:23 +0300

    UseState.stories.tsx

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+export default {
+
+    title: 'useState demo'
+}
+
+
+
+export const Example1 = () => {
+    console.log('Example1')
+    const [counter, setCounter] = useState(0)
+
+    return <>
+        <button onClick={()=> setCounter(counter +1)} >+</button>
+        {counter}
+    </>
+}
+
