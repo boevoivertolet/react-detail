7447a17c61b5e171f18a017846b0a7ec8c17bf4b
boevoivertolet <bjha78rus@yandex.ru>
2023-01-04 16:04:47 +0300

    UseEffect demo

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+
+export default {
+
+    title: 'UseEffect demo'
+}
+
+
+export const SimpleExample = () => {
+    console.log('SimpleExample')
+    const [counter, setCounter] = useState(1)
+
+    useEffect(() => {
+        console.log('useEffect')
+    })
+    return <>
+        Hello,{counter}
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+    </>
+}
+
--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -5,14 +5,20 @@ export default {
     title: 'useState demo'
 }
 
+function generateData() {
+    console.log('generateData')
+    return 1
+}
 
 
 export const Example1 = () => {
     console.log('Example1')
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(generateData)
+
+    const changer = (state: number) => state + 1
 
     return <>
-        <button onClick={()=> setCounter(counter +1)} >+</button>
+        <button onClick={() => setCounter(changer)}>+</button>
         {counter}
     </>
 }
